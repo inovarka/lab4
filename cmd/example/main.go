f114676f4a1babef54ce55270dec8f8a6c72dd63
Paul Tim <paveltimofeenko@gmail.com>
2020-12-26 16:38:17 +0300

    main.go reworked

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -5,25 +5,48 @@ import (
 	"flag"
 	"os"
 
+	"github.com/inovarka/lab4/commands"
 	"github.com/inovarka/lab4/engine"
 )
 
+var (
+	inputFile = flag.String("f", "", "Input file name")
+)
+
+func scanFile(inputFile *string, loop *engine.EventLoop) error {
+	var source io.Reader
+
+	if *inputFile != "" {
+		data, err := ioutil.ReadFile(*inputFile)
+		if err != nil {
+			return err
+		}
+		source = strings.NewReader(string(data))
+	} else {
+		return errors.New("No file")
+	}
+
+	scanner := bufio.NewScanner(source)
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+		cmd := commands.Parse(commandLine)
+		loop.Post(cmd)
+	}
+
+	return nil
+}
+
 func main() {
-	inputFile := flag.String("f", "", "Run file with EventLoop")
 	flag.Parse()
 
 	eventLoop := new(engine.EventLoop)
 	eventLoop.Start()
-	if input, err := os.Open(inputFile); err == nil {
-		defer input.Close()
-		scanner := bufio.NewScanner(input)
-		for scanner.Scan() {
-			commandLine := scanner.Text()
-			cmd := Parse(commandLine)
-			eventLoop.Post(cmd)
-		}
+
+	if err := scanFile(inputFile, eventLoop); err != nil {
+		_, _ = os.Stderr.WriteString(err.Error() + "\n")
 	}
 
 	eventLoop.AwaitFinish()
+}
 
 }
