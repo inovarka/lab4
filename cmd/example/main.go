67a7ac16b5add5ff360624e8c206da175a9fab48
Paul Tim <paveltimofeenko@gmail.com>
2020-12-26 18:07:46 +0300

    main.go simplify

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
+	"io"
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/inovarka/lab4/commands"
 	"github.com/inovarka/lab4/engine"
 )
 
 var (
-	inputFile = flag.String("f", "", "Input file name")
+	inputFile = flag.String("f", "", "example")
 )
 
 func scanFile(inputFile *string, loop *engine.EventLoop) error {
@@ -37,16 +41,33 @@ func scanFile(inputFile *string, loop *engine.EventLoop) error {
 }
 
 func main() {
-	flag.Parse()
 
+	filename := "example.txt"
 	eventLoop := new(engine.EventLoop)
 	eventLoop.Start()
-
-	if err := scanFile(inputFile, eventLoop); err != nil {
-		_, _ = os.Stderr.WriteString(err.Error() + "\n")
+	if input, err := os.Open(filename); err == nil {
+		defer input.Close()
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			commandLine := scanner.Text()
+			cmd := commands.Parse(commandLine)
+			eventLoop.Post(cmd)
+		}
+	} else {
+		panic(err)
 	}
-
 	eventLoop.AwaitFinish()
-}
+	// Doesn't execute after finish
+	eventLoop.Post(commands.Parse("print never_run"))
 
+	//flag.Parse()
+
+	//eventLoop := new(engine.EventLoop)
+	//eventLoop.Start()
+
+	//if err := scanFile(inputFile, eventLoop); err != nil {
+	//	_, _ = os.Stderr.WriteString(err.Error() + "\n")
+	//}
+
+	//eventLoop.AwaitFinish()
 }
