059d03107a3be4106a7165cbdcdcaf711e5f282f
Paul Tim <paveltimofeenko@gmail.com>
2020-12-26 16:20:38 +0300

    parse func from main removed

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,19 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"os"
-	"strings"
 
-	"github.com/inovarka/lab4/commands"
 	"github.com/inovarka/lab4/engine"
 )
-
-func parse(cmdLine string) engine.Command {
-	cmdFields := strings.Fields(cmdLine)
-	cmdName := cmdFields[0]
-	args := cmdFields[1:]
-	res := commands.Construct(cmdName, args)
-	return res
-}
 
 func main() {
 	inputFile := flag.String("f", "", "Run file with EventLoop")
@@ -27,13 +17,13 @@ func main() {
 	if input, err := os.Open(inputFile); err == nil {
 		defer input.Close()
 		scanner := bufio.NewScanner(input)
- 		for scanner.Scan() {
- 			commandLine := scanner.Text()
- 			cmd := parse(commandLine) 
- 			eventLoop.Post(cmd)
- 		}
+		for scanner.Scan() {
+			commandLine := scanner.Text()
+			cmd := Parse(commandLine)
+			eventLoop.Post(cmd)
+		}
 	}
-		
+
 	eventLoop.AwaitFinish()
 
 }
